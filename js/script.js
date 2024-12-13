04f7a9b5191aae8dccad145c54942b929a520fd5
DariuszSm <dariuszs2@nycstudents.net>
2024-12-13 01:47:12 -0500

    add progress

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
+var chosenPosts = []
+
 function init() {
     
 }
 
 async function async_init() {
-    workJson = await getResponse()
+    await get_posts()
+
     buildPage()
 }
 
-async function getResponse() {
-    var textResponse = await fetch('https://jsonplaceholder.typicode.com/users')
+async function get_posts() {
+    for (let i=1; i<=10; i++) {
+        postJson = await getResponse('https://jsonplaceholder.typicode.com/posts/' + Math.floor(Math.random()*100+1))
+        chosenPosts.push(postJson)
+    }
+
+
+}
+
+async function getResponse(url) {
+    var textResponse = await fetch(url)
     .catch(error => {
         return null
     })
@@ -16,9 +28,27 @@ async function getResponse() {
     return responseJson
 }
 
+function updatePostsPage() {
+    $("#posting-space").html("")
+    for (post in chosenPosts) {
+        console.log(chosenPosts[post])
+        /* 
+        <h1 class="post-title"></h1>
+        <p class="post-user"></p>
+        <p class="post-content"></p>
+        */
+        
+        var title = `<h1>${chosenPosts[post].title}</h1>`
+        var user = `<p>Username</p>`
+        var content = `<p>${chosenPosts[post].body}</p>`
+        
+        $("#posting-space").append(title, user, content)
+    }
+}
+
 function buildPage() {
 
-    
+    updatePostsPage()
 
 }
 
